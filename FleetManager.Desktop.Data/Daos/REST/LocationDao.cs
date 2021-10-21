ef4bfd6eb0f958e81e3c3da1a02ef8f1583b2aee
Josefine Paaske <1086237@ucn.dk>
2021-10-21 10:43:53 +0200

    Read metoder er lavet

--- FleetManager.Desktop.Data/Daos/REST/LocationDao.cs
+++ FleetManager.Desktop.Data/Daos/REST/LocationDao.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FleetManager.Desktop.Model;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,41 @@ namespace FleetManager.Desktop.Data.Daos.REST
     // 1. make the class inherit from the BaseDao class and use the relevant data context interface as type parameter
     // 2. implement the IDao interface in the class with the Location model class as type parameter
 
-    class LocationDao
+    class LocationDao : BaseDao<IDataContext<IRestClient>>, IDao<Location>
     {
+        public LocationDao(IDataContext<IRestClient> dataContext) : base(dataContext)
+        {
+        }
+
+        public Location Create(Location model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Location model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Location> Read()
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Locations", Method.GET);
+            IRestResponse<IEnumerable<Location>> response = client.Get<IEnumerable<Location>>(request);
+            return response.Data;
+        }
+
+        public IEnumerable<Location> Read(Func<Location, bool> predicate)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Locations", Method.GET);
+            IRestResponse<IEnumerable<Location>> response = client.Get<IEnumerable<Location>>(request);
+            return response.Data.Where(predicate);
+        }
+
+        public bool Update(Location model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
